4026a952b74ebab88ad3e6acdb97790a9ce0c132
Sanju Rajput <sanju.coder.agr@gmail.com>
2025-01-07 15:29:36 +0530

    added jenkins files

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-console.log("Hii, Mom")
-console.log('added jenkins file')
+console.log("Hii, Mom");
+console.log("added jenkins file");
 
+const express = require("express");
 
+const app = express();
+
+app.get("/", async (req, res) => {
+  try {
+    res.send("Hii, server!!");
+  } catch (errr) {
+    console.log("errpr on", err);
+  }
+});
+
+app.listen(5000, (err) => {
+  if (err) throw new Error(err);
+  console.log(`server is running on port: ${5000}`);
+});

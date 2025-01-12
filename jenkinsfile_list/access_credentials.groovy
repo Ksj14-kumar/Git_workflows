835c169293b571b6bc019bf6e840ebf7cb46abe4
Sanju Rajput <sanju.coder.agr@gmail.com>
2025-01-12 16:33:57 +0530

    added docker file

--- jenkinsfile_list/access_credentials.groovy
+++ jenkinsfile_list/access_credentials.groovy
@@ -20,5 +20,22 @@ pipeline {
                 echo "this is access token value is: ${env.testing_token}"
             }
         }
+    stage('usernamePassword') {
+      steps {
+        script {
+          withCredentials([
+            usernamePassword(credentialsId: 'gitlab',
+              usernameVariable: 'username',
+              passwordVariable: 'password')
+          ]) {
+            print 'username=' + username + 'password=' + password
+
+            print 'username.collect { it }=' + username.collect { it }
+            print 'password.collect { it }=' + password.collect { it }
+          }
+        }
+      }
+    }
      }
+
 }
